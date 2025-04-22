aa192a62129094d47c952ba0bdbd9174a7115087
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-04-22 18:05:52 +0700

    commit perbaikan ui
    DI HSUP

--- main_menu.gd
+++ main_menu.gd
@@ -4,5 +4,5 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://StartGame.tscn")
 func _on_option_pressed() -> void:
 	get_tree().change_scene_to_file("res://option.tscn")
-func _on_exit_button_pressed() -> void:
+func _on_exit_game_pressed() -> void:
 	get_tree().quit()
