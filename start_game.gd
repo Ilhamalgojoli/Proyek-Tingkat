307a33e20def469af13d0ea8403d4899bcae440e
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-04-23 20:24:36 +0700

    Pindah repo

--- start_game.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_start_play_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://ChooseLevel.tscn")
-func _on_ar_pressed() -> void:
-	get_tree().change_scene_to_file("res://ar_mode.tscn")
-func _on_back_to_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
